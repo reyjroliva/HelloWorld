da88afe82f425aafb4555990ea1fe7933b4b58ec
reyjroliva <reyjroliva@ufl.edu>
2019-02-23 22:43:43 -0500

    Made hello world

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -10,8 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var HelloWorld: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        HelloWorld.text = "Hello, world!"
+        HelloWorld.frame = CGRect(x: 0, y: self.view.frame.height / 2, width: self.view.frame.width, height: self.view.frame.height / 10)
+        
         
         
         // Do any additional setup after loading the view, typically from a nib.
