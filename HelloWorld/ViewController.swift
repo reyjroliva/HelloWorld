3484c4f72be6a3cc1260d4dd03b5d265ccfa8a4c
reyjroliva <reyjroliva@ufl.edu>
2019-02-23 23:15:21 -0500

    Dolphin button

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var HelloWorld: UILabel!
+    @IBOutlet weak var btn: UIButton!
+    
+    var dlphn = false;
     
     
     override func viewDidLoad() {
@@ -19,7 +22,9 @@ class ViewController: UIViewController {
         HelloWorld.text = "Hello, world!"
         HelloWorld.frame = CGRect(x: 0, y: self.view.frame.height / 2, width: self.view.frame.width, height: self.view.frame.height / 10)
         
-        
+        btn.setTitle("Dolphin", for: .normal)
+        btn.frame = CGRect(x: HelloWorld.frame.width / 3, y: HelloWorld.frame.maxY + 10, width: HelloWorld.frame.width / 3, height: HelloWorld.frame.height)
+        btn.layer.cornerRadius = 25;
         
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -29,6 +34,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func makeDolphin(_ sender: Any) {
+        if(dlphn == true)
+        {
+            HelloWorld.text = "Not a dolphin"
+            HelloWorld.textColor = UIColor .black;
+        }
+        else
+        {
+            HelloWorld.text = "I'm a dolphin"
+            HelloWorld.textColor = UIColor .blue;
+        }
+        self.dlphn = (!self.dlphn)
+    }
+    
 }
 
